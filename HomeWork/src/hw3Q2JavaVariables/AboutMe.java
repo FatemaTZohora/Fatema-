eccdc658b78accbeeb42d780b456c2a3da76b354
FatemaTZohora <ftmtjzhr@gmail.com>
2022-01-13 22:31:31 -0500

    01132022 10:31pm

---
+++ HomeWork/src/hw3Q2JavaVariables/AboutMe.java
@@ -0,0 +1,28 @@
+package hw3Q2JavaVariables;
+public class AboutMe {
+public String myInfo{};
+public String name="Fatema Zohora";
+public int myYearlySalary=70000000;
+public char gender='F';
+public boolean usCitizen=true;
+public byte age=28;
+public short houseRent=2000;
+public long myBankBalance=900000000l;
+public float hight=5.5f;
+public double myGrade=3.3333;
+//here myInfo variable is declared as I didn't assign any value of it. 
+
+
+
+
+
+
+
+
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
