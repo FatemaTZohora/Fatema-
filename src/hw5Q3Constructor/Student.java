be70398d131c0b8ac27c71577c0824ebe6489919
FatemaTZohora <ftmtjzhr@gmail.com>
2022-01-20 12:27:23 -0500

    01202022 12:27pm

--- src/hw5Q3Constructor/Student.java
+++ src/hw5Q3Constructor/Student.java
@@ -1,24 +1,24 @@
 package hw5Q3Constructor;
 
 public class Student {
-public String stName;
-public int stID;
-public char sex;
-public boolean isProgrammer;
-public float grade;
-public Student() {
-	System.out.println("This is from default Constructor of Student class.");
-}
+	public String stName;
+	public int stID;
+	public char sex;
+	public boolean isProgrammer;
+	public float grade;
 
- public Student(String stName, int stID, char sex, boolean isProgrammer, float grade) {
-        this.stName = stName;
-        this.stID = stID;
-        this.sex = sex;
-        this.isProgrammer = isProgrammer;
-        this.grade = grade;
-        
-        System.out.println("\nStudent Name : "+ stName +"\nID: "+ stID +"\nSex : "+ sex + "\nJava Progrmmer? Ans: " + isProgrammer + "\nGrade= " + grade);
-}
+	public Student() {
+		System.out.println("This is from default Constructor of Student class.");
 	}
 
+	public Student(String stName, int stID, char sex, boolean isProgrammer, float grade) {
+		this.stName = stName;
+		this.stID = stID;
+		this.sex = sex;
+		this.isProgrammer = isProgrammer;
+		this.grade = grade;
 
+		System.out.println("\nStudent Name : " + stName + "\nID: " + stID + "\nSex : " + sex + "\nJava Progrmmer? Ans: "
+				+ isProgrammer + "\nGrade= " + grade);
+	}
+}
--- src/hw5Q3Constructor/StudentTest.java
+++ src/hw5Q3Constructor/StudentTest.java
@@ -7,23 +7,22 @@ public class StudentTest {
 		Student student01 = new Student();
 
 		System.out.println("---------------------------------------------");
-		
-		Student student02 = new Student("Fatema Tuj Zohora", 455 ,'f' , true , 3.976f);
-		
+		// 4 Parameterized Constructors initialized here:
+
+		Student student02 = new Student("Fatema Tuj Zohora", 455, 'f', true, 3.976f);
+
 		System.out.println("---------------------------------------------");
-		
-		Student student03 = new Student("Fabiha Islam", 955 ,'f' , false , 2.906f);
-		
+
+		Student student03 = new Student("Fabiha Islam", 955, 'f', false, 2.906f);
+
 		System.out.println("---------------------------------------------");
-		
-		Student student04 = new Student("Shoaib Islam", 345 ,'m' , false , 2.916f);
-		
+
+		Student student04 = new Student("Shoaib Islam", 345, 'm', false, 2.916f);
+
 		System.out.println("---------------------------------------------");
-		
-		
-		Student student05 = new Student("Mohammed Islam", 422 ,'m' , true , 1.976f);
-		
-		
+
+		Student student05 = new Student("Mohammed Islam", 422, 'm', true, 1.976f);
+
 	}
 
 }
