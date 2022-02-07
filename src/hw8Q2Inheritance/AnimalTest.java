fb42d1254f8a947a8e6f1cd7607ca3b1e79354b6
FatemaTZohora <ftmtjzhr@gmail.com>
2022-02-07 18:27:49 -0500

    02072022 6:27 pm

--- src/hw8Q2Inheritance/AnimalTest.java
+++ src/hw8Q2Inheritance/AnimalTest.java
@@ -8,31 +8,33 @@ public class AnimalTest {
 		BullDog bullDog = new BullDog();
 		bullDog.bullDogInfo();
 		bullDog.dogInfo();
-		// "BullDog extends Dog, Dog extends Mammal, Mammal extends Animal " They are
-		// Multi-level Inheritance.
-		// "BullDog extends Dog" This is single Inheritance.
-		// "Mammal extends Animal, Birds extends Animal, Reptile extends Animal they are
-		// called Hierarchical Inheritance.
+		/*
+		 * "BullDog extends Dog, Dog extends Mammal, Mammal extends Animal " They are
+		 * Multi-level Inheritance. "BullDog extends Dog" This is single Inheritance.
+		 * "Mammal extends Animal, Birds extends Animal, Reptile extends Animal they are
+		 * called Hierarchical Inheritance.
+		 */
 		System.out.println("\n----------------------------------------------------\n");
 		Cobra cobra = new Cobra();
 		cobra.cobraInfo();
 		cobra.snakeInfo();
-		// Here Cobra is a Child Class and Snake is a Parent Class
-		// "Cobra extends Snake" --This is single Inheritance.
-		// "Cobra extends Snake, Snake extends Reptile, Reptile extends Animal"-- they
-		// are called Multi-level Inheritance.
-		// Cobra extends Animal, Snake extends Animal, Reptile extends Animal"-- they
-		// are called Hierarchical Inheritance.
+		/*
+		 * Here Cobra is a Child Class and Snake is a Parent Class "Cobra extends Snake"
+		 * --This is single Inheritance.
+		 * "Cobra extends Snake, Snake extends Reptile, Reptile extends Animal"-- they
+		 * are called Multi-level Inheritance. Cobra extends Animal, Snake extends
+		 * Animal, Reptile extends Animal"-- they are called Hierarchical Inheritance.
+		 */
 		System.out.println("\n----------------------------------------------------\n");
 		Robin robin = new Robin();
 		robin.robinInfo();
 		robin.birdsInfo();
-		// Here Robin is a Child Class and Birds is a Parent Class
-		// "Robin extends Birds" --This is single Inheritance.
-		// "Robin extends Birds, Birds extends Animal"-- they are called Multi-level
-		// Inheritance.
-		// "Robin extends Animal, Birds extends Animal"-- they are called Hierarchical
-		// Inheritance.
+		/*
+		 * Here Robin is a Child Class and Birds is a Parent Class "Robin extends Birds"
+		 * --This is single Inheritance. "Robin extends Birds, Birds extends Animal"--
+		 * they are called Multi-level Inheritance. "Robin, Birds extends Animal"-- they
+		 * are called Hierarchical Inheritance.
+		 */
 		System.out.println("\n----------------------------------------------------\n");
 		Dog dog = new Dog();
 		dog.dogInfo();
